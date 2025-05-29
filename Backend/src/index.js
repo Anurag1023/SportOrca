c4e6532584626c423febc93561b4734fb27cbcc1
Anurag1023 <rajputanuragsingh10@gmail.com>
2025-05-29 15:30:59 +0530

    index 2.0

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -53,10 +53,10 @@ app.get('/api/short-matches', async (req, res) => {
 
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../Frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
   });
 }
 
